96a076447807510c30fb71a5a57c369b33c3de39
mahamoudbile <mahamoudbileciise@hotmail.com>
2022-09-23 14:31:59 +0200

    #bij vakken lorem vervangen

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="CSS/main.css">
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="CSS/swiper-bundle.min.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+
+		
+			<img src="IMG/c1img/logo.png" class="imgLogo">
+
+
+		</div>
+	</header>
+	<main>
+	
+		<div class="container1">
+		<!-- 	<div class="vakClass">
+				<h2> Schoolvakken leerjaar 1</h2>
+			</div> -->
+
+			<div class="popup" onclick="myFunction()" >
+				<h2>Schoolvakken leerjaar 1</h2>
+					<span class="popuptext" id="myPopup">In het 1e leerjaar krijg je negen vakken Op school. Vakken als NATIVE, WEB , PRO en PRA zijn de hoofdvakken
+					<br>Bij NATIVE leer in het 1e leerjaar je hoe je WIN-apps (Pc’s) ontwikkelt, later in de opleiding leer je meer over ontwikkelen van  mobiele-apps. 
+					<br>Bij WEB leer je hoe je websites of web-Apps programmeert op browser.
+					 <br>Bij het vak praktijk ondersteuning (afkorting PRO) krijg je bijna niks over programmeren of codes schrijven, maar meer over hoe je een projecten aan pakt of presenteert. 
+					<br>Het vak praktijkwerk (afkorting PRA) werkje in een kleine groepje van 2 tot 5 personen samen aan een project. Je praktiseert dan wat je in vakken NATIVE, WEB en PRO hebt geleerd.
+					Verder krijg je Nederlands, Engels, rekenen, studie loopbaan begeleiding(SLB) en POR.
+				</span>
+			</div>
+<!-- ==================================================== -->
+			<div class="SubPict">
+				<div class="test">
+					<img src="IMG/c1img/win.png" class="imgVak">
+					<p>	Native</p>
+				</div>
+
+				<div class="test1">
+					<img src="IMG/c1img/WEB.jpg" class="imgVak">
+					<p>	WEB</p>
+				</div>
+
+				<div class="test2">
+					<img src="IMG/c1img/pro.jpg" class="imgVak">
+					<p>	Praktijk ondersteuning</p>
+				</div>
+
+				<div class="test3">
+					<img src="IMG/c1img/PRA.jpeg" class="imgVak">
+					<p>	Praktijkwerk</p>
+				</div>
+
+				<div class="test5">
+					<img src="IMG/c1img/nl.jpg" class="imgVak">
+					<p>	Nederlands</p>
+				</div>
+
+				<div class="test6">
+					<img src="IMG/c1img/eng.png" class="imgVak">
+					<p>	Engels</p>
+				</div>
+
+				<div class="test7">
+					<img src="IMG/c1img/rek.png" class="imgVak">
+					<p>	Rekenen</p>
+				</div>
+
+				<div class="test8">
+					<img src="IMG/c1img/SLB.jpg" class="imgVak">
+					<p>	SLB</p>
+				</div>
+
+				<div class="test9">
+					<img src="IMG/c1img/por.jpg" class="imgVak">
+					<p class="POR">	POR</p>
+				</div>
+			</div>
+
+
+
+
+			<div class="ove">
+				<!-- <h2>Overgansnorm</h2> -->
+				<div class="popup1" onclick="myFunction1()" >
+				<h2>Overgangsnorm</h2>
+				<span class="popuptext1" id="myPopup1">In elk leerjaar zijn er twee blokken die half jaar duren. In het 1e deel van leerjaar 1 heet blok A en 2e deel blok B.
+				In het 1e leerjaar zijn er drie mogelijkheden als het gaat over de overgangsnorm.
+				<ul>
+					<li>Versnellen: Minimaal 90% leeruitkomst, Alle projecten van praktijkwerk zijn voldaan afgerond en alle feedbackmomenten van Nederlands, Engels en rekenen zijn ook voldaan afgerond. </li>
+					<li>-	Over naar het volgende blok: Minimaal 70% leeruitkomst, maximaal 1 projecten van praktijkwerk is niet voldaan afgerond en maximaal 1 feedbackmomenten van Nederlands, Engels en rekenen is ook voldaan afgerond</li>
+					<li>Blok opnieuw overdoen: minder dan 70% leeruitkomst is gelijk gezakt. </li>
+				</ul>
+				</span>
+			</div>
+				<h4>90% - 100%</h4>
+				<span></span>
+				<span></span>
+				<h4 class="h4OV">Versnellen</h4>
+
+				<h4>70% - 90%</h4>
+				<span></span>
+				<span></span>
+				<h4 class="h4OV">Door naar de volgende blok</h4>
+
+				<h4>0% - 70%</h4>
+				<span></span>
+				<span></span>
+				<h4 class="h4OV">Blok opnieuw overdoen</h4>
+			</div>
+
+			<!-- ============================================================================== -->
+			<div class="Docenten">
+				<h2>Smoelen boek van de docenten</h2>
+			</div>
+			<div class="slide-container swiper">
+				<div class="slide-content">
+					<div class="card-wrapper swiper-wrapper">
+						<div class="card swiper-slide">
+							<div class="image-content">
+								<span class="overlay"></span>
+
+								<div class="card-image">
+									<img src="IMG/c1img/docent.jpeg" alt="docent" class="card-img">
+								</div>
+							</div>
+							<div class="card-content">
+								<h2 class="name">Bart Roos1</h2>
+								<p class="description">Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Magni, similique illo harum. Sit, totam! Possimus optio doloremque consequatur alias accusantium similique nesciunt id.</p>
+							</div>						
+						</div>
+						<!-- =================2===== -->
+
+						<div class="card swiper-slide">
+							<div class="image-content">
+								<span class="overlay"></span>
+
+								<div class="card-image">
+									<img src="IMG/c1img/docent.jpeg" alt="docent" class="card-img">
+								</div>
+							</div>
+							<div class="card-content">
+								<h2 class="name">Bart Roos2</h2>
+								<p class="description">Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Magni, similique illo harum. Sit, totam! Possimus optio doloremque consequatur alias accusantium similique nesciunt id.</p>
+							</div>						
+						</div>
+
+						<!-- ===============3======= -->
+
+						<div class="card swiper-slide">
+							<div class="image-content">
+								<span class="overlay"></span>
+
+								<div class="card-image">
+									<img src="IMG/c1img/docent.jpeg" alt="docent" class="card-img">
+								</div>
+							</div>
+							<div class="card-content">
+								<h2 class="name">Bart Roos3</h2>
+								<p class="description">Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Magni, similique illo harum. Sit, totam! Possimus optio doloremque consequatur alias accusantium similique nesciunt id.</p>
+							</div>						
+						</div>
+						<!-- ===============4======= -->
+						<div class="card swiper-slide">
+							<div class="image-content">
+								<span class="overlay"></span>
+
+								<div class="card-image">
+									<img src="IMG/c1img/docent.jpeg" alt="docent" class="card-img">
+								</div>
+							</div>
+							<div class="card-content">
+								<h2 class="name">Bart Roos4</h2>
+								<p class="description">Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Magni, similique illo harum. Sit, totam! Possimus optio doloremque consequatur alias accusantium similique nesciunt id.</p>
+							</div>						
+						</div>
+
+						<!-- ===============5======= -->
+						<div class="card swiper-slide">
+							<div class="image-content">
+								<span class="overlay"></span>
+
+								<div class="card-image">
+									<img src="IMG/c1img/docent.jpeg" alt="docent" class="card-img">
+								</div>
+							</div>
+							<div class="card-content">
+								<h2 class="name">Bart Roos5</h2>
+								<p class="description">Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Magni, similique illo harum. Sit, totam! Possimus optio doloremque consequatur alias accusantium similique nesciunt id.</p>
+							</div>						
+						</div>
+
+					</div>
+				</div>
+				<div class="swiper-button-next"></div>
+		       <div class="swiper-button-prev"></div>
+		       <div class="swiper-pagination"></div>
+		       
+			</div>
+
+					
+			<!-- ======================================JV SCRIPT==================================== -->
+		</div>
+	</main>
+
+	<footer>
+		<div class="footer">
+
+		</div>
+	</footer>
+</body>
+
+
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/script.js"></script>
+
+<script>
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+</script>
+	
+</html>
