d9e4648593342bbe2037741b4817fa50b8152d2b
mahamoudbile <mahamoudbileciise@hotmail.com>
2022-09-29 13:56:22 +0200

    #pagina titel verandert
    Naam van de titel verandert van 'Document' naar 'www.ciise.nl'

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>www.Ciise.nl</title>
 	<link rel="stylesheet" type="text/css" href="CSS/main.css">
 	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="CSS/swiper-bundle.min.css">
